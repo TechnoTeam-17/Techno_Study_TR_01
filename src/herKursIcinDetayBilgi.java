98d3b76c97f3957f67ffb09ac7d049d1ec9c23a2
NergisRecel <nergisrecel@hotmail.com>
2023-09-08 00:47:07 +0200

    Anasayfada bulunan 3 adet 'Detayli Bilgi' linkinin calisip calismadigi ve acilan kurs sayfalarinda Technostudy logosu olup olmadigi assert edilmistir.

--- src/herKursIcinDetayBilgi.java
+++ src/herKursIcinDetayBilgi.java
@@ -3,7 +3,9 @@ import Utility.BaseDriverBrowser;
 import Utility.MyFunc;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
@@ -26,19 +28,43 @@ public class herKursIcinDetayBilgi extends BaseDriver{
             System.out.println("3 Kurs icin Detayli Bilgi linkleri = " + detayliBilgi.get(i).getAttribute("href"));
             Assert.assertEquals(detayliBilgi.get(0).getAttribute("href"),"https://techno.study/tr/data");
             Assert.assertEquals(detayliBilgi.get(1).getAttribute("href"),"https://techno.study/tr/sdet");
-            Assert.assertEquals(detayliBilgi.get(2).getAttribute("href"),"https://techno.study/tr/androidapp");
+            Assert.assertEquals(detayliBilgi.get(2).getAttribute("href"),"https://techno.study/tr/androidapp");}
 
-        }
 
-    //    WebElement sdet=driver.findElement(By.xpath("//*[contains(@href,'https://techno.study/tr/sdet')]"));
-     //   sdet.click();
-     //   WebElement android=driver.findElement(By.xpath("//*[contains(@href,'https://techno.study/tr/androidapp')]"));
-    //    android.click();
-     //  WebElement data=driver.findElement(By.xpath("//*[contains(@href,'https://techno.study/tr/data')]"));
-     //   data.click();
+     WebElement sdet=driver.findElement(By.xpath("//*[contains(@href,'https://techno.study/tr/sdet')]"));
+     sdet.click();
+        wait.until(ExpectedConditions.urlToBe("https://techno.study/tr/sdet"));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://techno.study/tr/sdet");
 
-    }
+     WebElement logo=driver.findElement(By.xpath("//img[@class='t228__imglogo ']"));
+     wait.until(ExpectedConditions.visibilityOf(logo));
+     Assert.assertTrue(logo.isDisplayed(),"Logo gorunmuyor");
 
-    }
+        driver.navigate().back();
+        wait.until(ExpectedConditions.urlToBe("https://techno.study/tr"));
+
+     WebElement android=driver.findElement(By.xpath("//*[contains(@href,'https://techno.study/tr/androidapp')]"));
+     android.click();
+
+        wait.until(ExpectedConditions.urlToBe("https://techno.study/tr/androidapp"));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://techno.study/tr/androidapp");
+
+        WebElement logo2=driver.findElement(By.xpath("//img[@class='t228__imglogo ']"));
+        wait.until(ExpectedConditions.visibilityOf(logo2));
+        Assert.assertTrue(logo2.isDisplayed(),"Logo gorunmuyor.");
+
+        driver.navigate().back();
+        wait.until(ExpectedConditions.urlToBe("https://techno.study/tr"));
+
+     WebElement data=driver.findElement(By.xpath("//*[contains(@href,'https://techno.study/tr/data')]"));
+     data.click();
+
+        wait.until(ExpectedConditions.urlToBe("https://techno.study/tr/data"));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://techno.study/tr/data");
+
+        WebElement logo3=driver.findElement(By.xpath("//img[@class='t228__imglogo ']"));
+        wait.until(ExpectedConditions.visibilityOf(logo3));
+        Assert.assertTrue(logo3.isDisplayed(),"Logo gorunmuyor.");
+    }}
 
 
